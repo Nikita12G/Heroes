acc3e79e4a2e89a25bc94f4c0e4432e52a470160
Nikita12G <87696400+Nikita12G@users.noreply.github.com>
2021-11-20 13:21:16 +0700

    Сreated table and network manager

---
+++ Heroues/Heroes/Servise/FetchImage.swift
@@ -0,0 +1,16 @@
+//
+//  FetchImage.swift
+//  Heroes
+//
+//  Created by Никита Гуляев on 20.11.2021.
+//
+import Foundation
+
+//class NetworkDataFetcher {
+//    
+//    let networkService = NetworkService()
+//    func fetchTracks() {
+// 
+//            }
+//        }
+
---
+++ Heroues/Heroes/ViewControllers/HeroViewController.swift
@@ -0,0 +1,25 @@
+//
+//  HeroViewController.swift
+//  Heroes
+//
+//  Created by Никита Гуляев on 18.11.2021.
+//
+
+import UIKit
+
+class HeroViewController: UIViewController {
+    @IBOutlet weak var heroImage: UIImageView!
+    
+    @IBOutlet var nameLabel: UIView!
+    
+    @IBOutlet weak var taglineLabel: UILabel!
+    
+    @IBOutlet weak var itemLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
